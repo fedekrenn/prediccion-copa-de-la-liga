5a870bde2d746cdf005888fa391adec7502c3176
Federico Krenn <fedekrenn@gmail.com>
2024-01-12 10:46:48 -0300

    Refactor getData function to handle error cases

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -2,19 +2,10 @@ import format from './format'
 import type { Table, GeneralTable } from '../types/cheerio'
 
 export default function getData(tablaDatos: Table, datosGenerales: GeneralTable) {
-  let datosFormateados
+  if (!tablaDatos) throw new Error('No se encontró el párrafo específico.')
 
-  if (tablaDatos !== undefined) {
-    const tabla = tablaDatos.next('table')
+  const tabla = tablaDatos.next('table')
+  if (tabla.length === 0) throw new Error('No se encontró la tabla hermana.')
 
-    if (tabla.length > 0) {
-      datosFormateados = format(tabla, datosGenerales)
-    } else {
-      console.log('No se encontró la tabla hermana.')
-    }
-  } else {
-    console.log('No se encontró el párrafo específico.')
-  }
-
-  return datosFormateados
+  return format(tabla, datosGenerales)
 }
