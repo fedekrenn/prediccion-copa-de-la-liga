2d0425571baef3c8138ce26ef769cc99ef071722
Federico Krenn <fedekrenn@gmail.com>
2024-01-26 00:03:22 -0300

    Add type definition for ValidPoints and handle invalid values in Row component

--- src/utils/calculatePartial.ts
+++ src/utils/calculatePartial.ts
@@ -1,6 +1,6 @@
 import type { TeamList } from '../types/tableFormat'
 
-const PARTIDOS_TOTALES = 14
+const PARTIDOS_TOTALES = 14 + 27
 
 export default function calculatePartial(arrayEquipos: TeamList) {
   return arrayEquipos?.map(infoEquipo => {
