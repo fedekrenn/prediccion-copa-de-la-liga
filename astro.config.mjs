c13f28863550238295f484b6c006b7bd9fdaed18
Federico Krenn <fedekrenn@gmail.com>
2024-01-29 13:24:42 -0300

    Testing bug 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
 
-// import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
-// export default defineConfig({
-//   output: 'server',
-//   adapter: vercel()
-// })
-
-export default defineConfig({})
+export default defineConfig({
+  output: 'server',
+  adapter: vercel()
+})
