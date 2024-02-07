558a5c8ff8b2349cd2d563e4c5f4272bb7d486c6
Federico Krenn <fedekrenn@gmail.com>
2024-02-07 15:50:42 -0300

    Fix calculation of porcentajeActual in calculatePartial.ts

--- src/utils/calculatePartial.ts
+++ src/utils/calculatePartial.ts
@@ -5,7 +5,7 @@ const PARTIDOS_TOTALES = 14 + 27
 export default function calculatePartial(arrayEquipos: TeamList) {
   return arrayEquipos.map(({ nombre, puntosTotales, partidosJugados }) => {
 
-    const porcentajeActual = Math.round(((puntosTotales / (partidosJugados * 3)) * 100).toFixed(2))
+    const porcentajeActual = Math.round((puntosTotales / (partidosJugados * 3)) * 100);
     const puntosEstimados = Math.round(porcentajeActual * PARTIDOS_TOTALES / 100 * 3)
 
     return {
