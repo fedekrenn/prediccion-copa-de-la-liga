80af92076bb3d9c821220695d975cba903a7767c
Fede Krenn <fedekrenn@gmail.com>
2024-04-08 19:52:32 -0300

    Add calculatePartial function to calculate future table predictions

--- tests/clasification.test.ts
+++ tests/clasification.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect } from 'vitest'
 import calculateClasification from '../src/utils/calculateClasification'
+import calculatePartial from '../src/utils/calculatePartial';
 
-describe('calculateClasification', () => {
+describe('---- Calculate future table ----', () => {
   it('Should calculate correct position', () => {
     expect(calculateClasification(1, false)).toBe('libertadores');
     expect(calculateClasification(2, false)).toBe('libertadores');
@@ -19,4 +20,30 @@ describe('calculateClasification', () => {
     expect(calculateClasification(0, false)).toBe('noClasificado');
     expect(calculateClasification(9, true)).toBe('descensoPromedios');
   });
+
+  it('Should calculate correct partial prediction', () => {
+    const arrayEquipos = [
+      { nombre: 'River', puntosTotales: 24, partidosJugados: 13, diferenciaGoles: 14 },
+      { nombre: 'Boca', puntosTotales: 22, partidosJugados: 12, diferenciaGoles: 8 },
+      { nombre: 'Racing', puntosTotales: 21, partidosJugados: 13, diferenciaGoles: 9 },
+      { nombre: 'Independiente', puntosTotales: 22, partidosJugados: 13, diferenciaGoles: 4 },
+      { nombre: 'San Lorenzo', puntosTotales: 15, partidosJugados: 13, diferenciaGoles: -4 },
+      { nombre: 'Huracan', puntosTotales: 13, partidosJugados: 13, diferenciaGoles: -4 },
+      { nombre: 'Estudiantes', puntosTotales: 21, partidosJugados: 12, diferenciaGoles: 8 },
+      { nombre: 'Instituto', puntosTotales: 17, partidosJugados: 13, diferenciaGoles: 3 }
+    ]
+
+    const prediction = [
+      { nombre: 'River', porcentajeActual: 62, puntosEstimados: 76 },
+      { nombre: 'Boca', porcentajeActual: 61, puntosEstimados: 75 },
+      { nombre: 'Racing', porcentajeActual: 54, puntosEstimados: 66 },
+      { nombre: 'Independiente', porcentajeActual: 56, puntosEstimados: 69 },
+      { nombre: 'San Lorenzo', porcentajeActual: 38, puntosEstimados: 47 },
+      { nombre: 'Huracan', porcentajeActual: 33, puntosEstimados: 41 },
+      { nombre: 'Estudiantes', porcentajeActual: 58, puntosEstimados: 71 },
+      { nombre: 'Instituto', porcentajeActual: 44, puntosEstimados: 54 }
+    ]
+
+    expect(calculatePartial(arrayEquipos)).toEqual(prediction);
+  })
 })
