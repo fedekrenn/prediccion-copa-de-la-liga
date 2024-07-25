297182ff25d89ff62141e07c46eadaf472ed5f5c
Fede Krenn <fedekrenn@gmail.com>
2024-07-24 23:09:38 -0300

    refactor: Adjust tests to new lenguage

--- tests/clasification.test.ts
+++ tests/clasification.test.ts
@@ -23,25 +23,25 @@ describe('---- Calculate future table ----', () => {
 
   it('Should calculate correct partial prediction', () => {
     const arrayEquipos = [
-      { nombre: 'River', puntosTotales: 24, partidosJugados: 13, diferenciaGoles: 14, img: '' },
-      { nombre: 'Boca', puntosTotales: 22, partidosJugados: 12, diferenciaGoles: 8, img: '' },
-      { nombre: 'Racing', puntosTotales: 21, partidosJugados: 13, diferenciaGoles: 9, img: '' },
-      { nombre: 'Independiente', puntosTotales: 22, partidosJugados: 13, diferenciaGoles: 4, img: '' },
-      { nombre: 'San Lorenzo', puntosTotales: 15, partidosJugados: 13, diferenciaGoles: -4, img: '' },
-      { nombre: 'Huracan', puntosTotales: 13, partidosJugados: 13, diferenciaGoles: -4, img: '' },
-      { nombre: 'Estudiantes', puntosTotales: 21, partidosJugados: 12, diferenciaGoles: 8, img: '' },
-      { nombre: 'Instituto', puntosTotales: 17, partidosJugados: 13, diferenciaGoles: 3, img: '' }
+      { name: 'River', totalPoints: 24, playedMatches: 13, goalsDifference: 14 },
+      { name: 'Boca', totalPoints: 22, playedMatches: 12, goalsDifference: 8 },
+      { name: 'Racing', totalPoints: 21, playedMatches: 13, goalsDifference: 9 },
+      { name: 'Independiente', totalPoints: 22, playedMatches: 13, goalsDifference: 4 },
+      { name: 'San Lorenzo', totalPoints: 15, playedMatches: 13, goalsDifference: -4 },
+      { name: 'Huracan', totalPoints: 13, playedMatches: 13, goalsDifference: -4 },
+      { name: 'Estudiantes', totalPoints: 21, playedMatches: 12, goalsDifference: 8 },
+      { name: 'Instituto', totalPoints: 17, playedMatches: 13, goalsDifference: 3 }
     ]
 
     const prediction = [
-      { nombre: 'River', porcentajeActual: 62, puntosEstimados: 26 },
-      { nombre: 'Boca', porcentajeActual: 61, puntosEstimados: 27 },
-      { nombre: 'Racing', porcentajeActual: 54, puntosEstimados: 23 },
-      { nombre: 'Independiente', porcentajeActual: 56, puntosEstimados: 24 },
-      { nombre: 'San Lorenzo', porcentajeActual: 38, puntosEstimados: 16 },
-      { nombre: 'Huracan', porcentajeActual: 33, puntosEstimados: 14 },
-      { nombre: 'Estudiantes', porcentajeActual: 58, puntosEstimados: 26 },
-      { nombre: 'Instituto', porcentajeActual: 44, puntosEstimados: 18 }
+      { name: 'River', effectivityPorcentage: 62, estimatedTotalPoints: 26 },
+      { name: 'Boca', effectivityPorcentage: 61, estimatedTotalPoints: 27 },
+      { name: 'Racing', effectivityPorcentage: 54, estimatedTotalPoints: 23 },
+      { name: 'Independiente', effectivityPorcentage: 56, estimatedTotalPoints: 24 },
+      { name: 'San Lorenzo', effectivityPorcentage: 38, estimatedTotalPoints: 16 },
+      { name: 'Huracan', effectivityPorcentage: 33, estimatedTotalPoints: 14 },
+      { name: 'Estudiantes', effectivityPorcentage: 58, estimatedTotalPoints: 26 },
+      { name: 'Instituto', effectivityPorcentage: 44, estimatedTotalPoints: 18 }
     ]
 
     expect(calculatePartial(arrayEquipos)).toEqual(prediction);
