a69a717160805afaf459ea2f1b543013f18e83c4
Fede Krenn <fedekrenn@gmail.com>
2024-12-17 19:29:37 -0300

    feat: Enhance team info API with token verification and user ID management

--- src/pages/api/team-info.json.ts
+++ src/pages/api/team-info.json.ts
@@ -1,19 +1,39 @@
-import main from '@services/main'
+import main from "@services/main";
+import { verifyToken } from "@libs/auth";
+import type { APIRoute } from "astro";
 
-export async function GET() {
+export const GET: APIRoute = async ({ request }) => {
+  const token = request.headers.get("Authorization");
+
+  if (!token) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+      statusText: "Unauthorized",
+    });
+  }
+
+  const payload = verifyToken(token);
+
+  if (!payload) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+      statusText: "Unauthorized",
+    });
+  }
+
   try {
-    const data = await main()
+    const data = await main();
 
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
-    })
+    });
   } catch (error: any) {
     return new Response(null, {
       status: 500,
-      statusText: 'Error al obtener la información del servidor',
-    })
+      statusText: "Error al obtener la información del servidor",
+    });
   }
-}
+};
--- src/pages/lib/users.ts
+++ src/pages/lib/users.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 
 type User = {
+  id: `${string}-${string}-${string}-${string}-${string}`;
   email: string;
   password: string;
 };
@@ -9,15 +10,21 @@ const users: User[] = [];
 
 export const addUser = async (email: string, password: string) => {
   const hashedPassword = await bcrypt.hash(password, 8);
-  const user = { email, password: hashedPassword };
+  const id = crypto.randomUUID();
+
+  const user = { id, email, password: hashedPassword };
+
   users.push(user);
-  return user;
+  return { id, email };
 };
 
 export const getUserByEmail = async (email: string) => {
   return users.find((user) => user.email === email);
 };
 
-export const verifyPassword = async (password: string, hashedPassword: string) => {
+export const verifyPassword = async (
+  password: string,
+  hashedPassword: string
+) => {
   return bcrypt.compare(password, hashedPassword);
-}
+};
