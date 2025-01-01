b0ba907f89fc6d6d176f490a0773146c86faeb09
Fede Krenn <fedekrenn@gmail.com>
2025-01-01 16:26:35 -0300

    feat: Implement custom error handling in PredictionController and update API response status management

--- src/controllers/Prediction.ts
+++ src/controllers/Prediction.ts
@@ -1,10 +1,20 @@
 import { main } from "@services/main";
 
+class CustomError extends Error {
+  status: number;
+  statusText: string;
+
+  constructor(message: string, status: number, statusText: string) {
+    super(message);
+    this.status = status;
+    this.statusText = statusText;
+  }
+}
+
 export class PredictionController {
   static async getFullPrediction() {
     try {
-      const prediction = await main();
-      return prediction;
+      return await main();
     } catch (error) {
       throw error;
     }
@@ -16,7 +26,11 @@ export class PredictionController {
       const result = prediction.find((team) => team.position === position);
 
       if (!result) {
-        throw new Error("You must provide a valid position from 1 to 28");
+        throw new CustomError(
+          "You must provide a valid position from 1 to 28",
+          400,
+          "Bad Request"
+        );
       }
 
       return result;
@@ -28,12 +42,17 @@ export class PredictionController {
   static async getPredictionByTeamName(teamName: string) {
     try {
       const prediction = await main();
+
       const result = prediction.filter((team) =>
         team.name.toLowerCase().includes(teamName.toLowerCase())
       );
 
       if (result.length === 0) {
-        throw new Error("You must provide a valid team name");
+        throw new CustomError(
+          "You must provide a valid team name",
+          400,
+          "Bad Request"
+        );
       }
 
       return result;
@@ -45,13 +64,16 @@ export class PredictionController {
   static async getTeamsInClassification(classification: string) {
     try {
       const prediction = await main();
+
       const result = prediction.filter((team) =>
         team.classification.toLowerCase().includes(classification.toLowerCase())
       );
 
       if (result.length === 0) {
-        throw new Error(
-          "You must provide a valid classification: 'Libertadores', 'Sudamericana', 'noClasificado', 'descensoPorTabla' o 'descensoPromedios'"
+        throw new CustomError(
+          "You must provide a valid classification: 'Libertadores', 'Sudamericana', 'noClasificado', 'descensoPorTabla' o 'descensoPromedios'",
+          400,
+          "Bad Request"
         );
       }
 
--- src/pages/api/prediction.ts
+++ src/pages/api/prediction.ts
@@ -43,8 +43,8 @@ export const GET: APIRoute = async ({ request }) => {
         });
       } catch (error: any) {
         return new Response(JSON.stringify({ error: error.message }), {
-          status: 400,
-          statusText: "Bad Request",
+          status: error.status || 500,
+          statusText: error.statusText || "Server Error",
         });
       }
     }
@@ -61,8 +61,8 @@ export const GET: APIRoute = async ({ request }) => {
         });
       } catch (error: any) {
         return new Response(JSON.stringify({ error: error.message }), {
-          status: 400,
-          statusText: "Bad Request",
+          status: error.status || 500,
+          statusText: error.statusText || "Server Error",
         });
       }
     }
@@ -81,8 +81,8 @@ export const GET: APIRoute = async ({ request }) => {
         });
       } catch (error: any) {
         return new Response(JSON.stringify({ error: error.message }), {
-          status: 400,
-          statusText: "Bad Request",
+          status: error.status || 500,
+          statusText: error.statusText || "Server Error",
         });
       }
     }
