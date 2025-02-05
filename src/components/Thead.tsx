a6a1ecaceb52604e5e389183e0b229817f12c4cb
Federico Krenn <fedekrenn@gmail.com>
2025-02-04 21:17:04 -0300

    feat: Update table header abbreviations for improved clarity

--- src/components/Thead.tsx
+++ src/components/Thead.tsx
@@ -25,19 +25,19 @@ export default function Thead({
   return (
     <thead>
       <tr>
-        <th className="font-thin text-xs">Pos</th>
+        <th className="font-thin text-xs">POS</th>
         <th className="w-[150px] sm:w-[220px] font-thin text-xs">Equipo</th>
         <th title="Ordenar por efectividad" onClick={sortByEfectivity}>
-          <FilterHead filterOrder={efectivitySort} title="Efectividad" />
+          <FilterHead filterOrder={efectivitySort} title="EFC" />
         </th>
         <th title="Ordenar por puntos estimados" onClick={sortByPoints}>
-          <FilterHead filterOrder={pointsSort} title="Puntos" />
+          <FilterHead filterOrder={pointsSort} title="PTS" />
         </th>
         <th title="Ordenar por partidos jugados" onClick={sortByPlayedMatches}>
           <FilterHead filterOrder={playedMatchesSort} title="PJ" />
         </th>
         <th title="Ordenar por promedio estimado" onClick={sortByAverage}>
-          <FilterHead filterOrder={averageSort} title="Promedio" />
+          <FilterHead filterOrder={averageSort} title="PROM" />
         </th>
       </tr>
     </thead>
